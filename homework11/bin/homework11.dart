5673611c879ac4bfafc60b9304056851b2eaface
shathalrowaisan <shathalrowaisan@gmail.com>
2022-11-21 21:45:17 +0300

    shatha-hw-11

---
+++ homework11/bin/homework11.dart
@@ -0,0 +1,44 @@
+import 'package:homework11/homework11.dart' as homework11;
+import 'package:homework11/homework11.dart';
+
+void main(List<String> arguments) {
+
+  // 1
+  checkAllergy(score: 34);
+
+  // 2
+  printNumbers(list: [1,2,3,5,6,7]);
+
+  // 3
+  isPalindrome(text: "reviver");
+
+  // 4
+  findAllDevidors();
+
+  // 5
+  randomNumbers();
+
+  // 6
+  print("common Items of [3,5,3,8,9] and [3,5,3,8,9,5,7] : ");
+  print(commonItems(list1: [3,5,3,8,9] , list2: [3,5,3,8,9,5,7]));
+
+  // 7
+  backwards();
+
+  // 8
+  game();
+
+  // 9
+  RockPaperScissors();
+
+  // 10
+  passwordGenerator();
+
+  // 11
+  birthdays();
+
+  // 12
+  print(numberOfList(list: [3,5,3,8,9]));
+
+
+}
---
+++ homework11/test/homework11_test.dart
@@ -0,0 +1,6 @@
+import 'package:homework11/homework11.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
