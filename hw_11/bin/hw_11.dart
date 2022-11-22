9bb3a31dc463f3151c43a51dec7123f92ba6a889
khaled <khaled1422_8@gmail.com>
2022-11-22 10:10:28 +0300

    ff

---
+++ hw_11/bin/hw_11.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  number();
+}
+
+// List<String> Allergies = [
+//   "eggs",
+//   "peanuts",
+//   "shellfish",
+//   "strawberries",
+//   "tomatoes",
+//   "chocolate",
+//   "pollen",
+//   "cats"
+// ];
+
+Allergies_p() {}
+
+number() {
+  var F = Random().nextInt(50);
+  print(F);
+  if (F > 25) {
+    print("the number biger");
+  } else {
+    print("Small number");
+  }
+}
+// number_printer(int numb){
+  
+  
+// }
